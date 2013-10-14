7fb094e7c12940c1890f19068fdc36891840e163
unknown <Vorona@Slav.P>
2013-10-14 23:04:31 +0400

    comments added

--- hw2/SwapColumn/SwapColumn/Program.cs
+++ hw2/SwapColumn/SwapColumn/Program.cs
@@ -7,22 +7,15 @@ namespace SwapColumn
 {
     public class Program
     {
-        public static void PrintMatrix(int[,] array)
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.Write(array[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-        }
-
+        /// <summary>
+        /// Method is used to change two columns by places
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
         public static void SwapColumn(int [,] array, int i, int j)
         {
-            for (int k = 0; k < 3; k++)
+            for (int k = 0; k < array.GetLength(0); k++)
             {
                 int temp = array[k, i];
                 array[k, i] = array[k, j];
@@ -30,13 +23,17 @@ namespace SwapColumn
             }
         }
 
+        /// <summary>
+        /// Method is used to sort first elements of columns
+        /// </summary>
+        /// <param name="array"></param>
         public static void Sort(int[,] array)
         {
             Boolean flag = true;
             while (flag)
             {
                 flag = false;
-                for (int i = 0; i < 2; i++)
+                for (int i = 0; i < array.GetLength(0) - 1; i++)
                 {
                     if (array[0, i] > array[0, i + 1])
                     {
@@ -46,14 +43,9 @@ namespace SwapColumn
                 }
             }
         }
+
         static void Main(string[] args)
         {
-            int[,] array = { { 3, 2, 1 }, { 4, 5, 6 }, { 7, 8, 9 } };
-            Console.WriteLine("Before : \n");
-            PrintMatrix(array);
-            Sort(array);
-            Console.WriteLine("After : \n");
-            PrintMatrix(array);
         }
     }
 }
--- hw2/SwapColumn/SwapColumnTests/SwapColumnTest.cs
+++ hw2/SwapColumn/SwapColumnTests/SwapColumnTest.cs
@@ -16,9 +16,9 @@ namespace SwapColumnTests
             int[,] array1 = { { 9, 8, 7 }, { 5, 4, 3 }, { 6, 2, 1 } };
             int[,] array2 = { { 7, 8, 9 }, { 3, 4, 5 }, { 1, 2, 6 } };
             Program.Sort(array1);
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < array1.GetLength(0); i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < array2.GetLength(0); j++)
                 {
                     Assert.AreEqual(array1[i, j], array2[i, j]);
                 }
