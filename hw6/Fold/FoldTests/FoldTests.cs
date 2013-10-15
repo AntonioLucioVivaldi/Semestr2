a2d0aaad8faef7866cf45b8d8bcbf431ad66a943
unknown <Vorona@Slav.P>
2013-10-15 21:00:56 +0400

    hw6

--- hw6/Fold/FoldTests/FoldTests.cs
+++ hw6/Fold/FoldTests/FoldTests.cs
@@ -11,11 +11,19 @@ namespace FoldTests
     public class FoldTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestFold()
         {
             List<int> list = new List<int>() { 1, 2, 3 };
             int value = FoldFunction.Fold(list, 1, (acc, elem) => acc * elem);
             Assert.AreEqual(value, 6);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmptyListException))]
+        public void TestEmptyListException()
+        {
+            List<int> list = new List<int>();
+            int value = FoldFunction.Fold(list, 2, (acc, elem) => acc * elem);
         }
     }
 }
